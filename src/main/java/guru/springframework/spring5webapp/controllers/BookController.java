0ed0a73dbd5457833d51673ae381aac476d41e28
memynamemystatus@gmail.com <dhanashekaragowda.sm@hcl.com>
2021-02-03 17:09:08 +0530

    segregated list.html file

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5webapp.controllers;
 
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,17 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BookController {
 
-    private final BookRepository bookRepository;
+    ///*@Autowired
+    private BookRepository bookRepository;
 
     public BookController(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
     @RequestMapping("/books")
-    public String getBooks(Model model){
+    public String getBooks(Model model) {
 
         model.addAttribute("books", bookRepository.findAll());
-
+        System.out.println("Data " + bookRepository.findAll());
         return "books/list";
     }
 }
